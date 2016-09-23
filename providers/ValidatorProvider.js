cc0e0bd19779149fc566671519e55eb27d3a5574
Harminder Virk <virk.officials@gmail.com>
2016-09-24 00:09:33 +0530

    refactor(validator): wrap unique rule in try/catch
    try/catch block makes sure to add the rule only when database provider
    is loaded. Later will move it to boot method, right now backward
    compatibility is required

--- providers/ValidatorProvider.js
+++ providers/ValidatorProvider.js
@@ -10,12 +10,19 @@ const ServiceProvider = require('adonis-fold').ServiceProvider
 const ExtendedRules = require('../src/ExtendedRules')
 
 class ValidatorProvider extends ServiceProvider {
+
   * register () {
     this.app.singleton('Adonis/Addons/Validator', function (app) {
-      const Database = app.use('Adonis/Src/Database')
-      const extendedRules = new ExtendedRules(Database)
       const validator = require('../src/Validator')
-      validator.extend('unique', extendedRules.unique.bind(extendedRules), '{{field}} has already been taken by someone else')
+      /**
+       * Wrap database unique rule inside a try/catch block
+       * incase someone is not using Lucid
+       */
+      try {
+        const Database = app.use('Adonis/Src/Database')
+        const extendedRules = new ExtendedRules(Database)
+        validator.extend('unique', extendedRules.unique.bind(extendedRules), '{{field}} has already been taken by someone else')
+      } catch (e) {}
       return validator
     })
   }

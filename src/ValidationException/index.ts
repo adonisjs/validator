4ef075f4552c921f4badb46c4026953991a8605f
Harminder virk <virk.officials@gmail.com>
2020-02-15 09:33:30 +0530

    fix(validationexception): extend base exception class

--- src/ValidationException/index.ts
+++ src/ValidationException/index.ts
@@ -8,21 +8,33 @@
 */
 
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { Exception } from '@poppinss/utils'
 
 /**
  * Validation exception to self handle and send response
  */
-export class ValidationException {
-  constructor (public flashToSession: boolean, public messages?: any) {}
+export class ValidationException extends Exception {
+  constructor (public flashToSession: boolean, public messages?: any) {
+    super('Validation Exception', 422, 'E_VALIDATION_FAILURE')
+  }
 
   public async handle (error: ValidationException, ctx: HttpContextContract) {
     if (!error.flashToSession || !ctx['session']) {
-      return ctx.response.status(422).send(error.messages)
+      return ctx.response.status(error.status).send(error.messages)
     }
 
     const session = ctx['session']
+
+    /**
+     * Flash all input, except the `_csrf_token`.
+     */
     session.flashExcept(['_csrf_token'])
+
+    /**
+     * Flash errors
+     */
     ctx['session'].flash('errors', error.messages)
+
     ctx.response.redirect('back', true)
   }
 }

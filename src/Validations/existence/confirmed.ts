176d6bcb2d35bd479bf9608223d012ab84b74048
Harminder virk <virk.officials@gmail.com>
2020-04-20 15:26:20 +0530

    feat: add confirmed rule

---
+++ src/Validations/existence/confirmed.ts
@@ -0,0 +1,51 @@
+/*
+ * @adonisjs/validator
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+import { SyncValidation } from '@ioc:Adonis/Core/Validator'
+import { exists, getFieldValue } from '../../Validator/helpers'
+
+const RULE_NAME = 'confirmed'
+const DEFAULT_MESSAGE = 'confirmed validation failed'
+
+/**
+ * Ensures that the field value has been confirmed using `{{field}}_confirmation` field.
+ * Useful for password confirmation.
+ */
+export const confirmed: SyncValidation = {
+  compile () {
+    return {
+      allowUndefineds: true,
+      async: false,
+      name: RULE_NAME,
+      compiledOptions: undefined,
+    }
+  },
+  validate (value, _, { errorReporter, field, pointer, arrayExpressionPointer, root, tip }) {
+    if (!exists(value)) {
+      return
+    }
+
+    /**
+     * We check to same value is not the type. Since it's possible that the original
+     * value is casted using a sub rule like `number`. However, the confirmed
+     * value is still a string.
+     *
+     * For example:
+     * age: schema.number([ rules.confirmed() ])
+     *
+     * Over HTTP the `age` and the `age_confirmation` field will both be strings. However,
+     * the `number` rule will cast the `age` to a number. So the type check with the
+     * confirmed rule will fail.
+     */
+    // eslint-disable-next-line eqeqeq
+    if (getFieldValue(`${field}_confirmation`, root, tip) != value) {
+      errorReporter.report(`${pointer}_confirmation`, RULE_NAME, DEFAULT_MESSAGE, arrayExpressionPointer)
+    }
+  },
+}
---
+++ test/validations/confirmed.spec.ts
@@ -0,0 +1,132 @@
+/*
+ * @adonisjs/validator
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+import test from 'japa'
+
+import { rules } from '../../src/Rules'
+import { ApiErrorReporter } from '../../src/ErrorReporter'
+import { confirmed } from '../../src/Validations/existence/confirmed'
+
+function compile () {
+  return confirmed.compile('literal', 'string', rules.confirmed().options)
+}
+
+test.group('Confirmed', () => {
+  test('ignore validation when original value is missing', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate(null, compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'password',
+      tip: {},
+      root: {},
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [])
+  })
+
+  test('report error when original value is present and confirmation field is missing', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate('secret', compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'password',
+      tip: {},
+      root: {},
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [{
+      field: 'password_confirmation',
+      rule: 'confirmed',
+      message: 'confirmed validation failed',
+    }])
+  })
+
+  test('report error when both fields are present but has different value', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate('secret', compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'password',
+      tip: {
+        password_confirmation: 'supersecret',
+      },
+      root: {},
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [{
+      field: 'password_confirmation',
+      rule: 'confirmed',
+      message: 'confirmed validation failed',
+    }])
+  })
+
+  test('work fine when field values are same', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate('secret', compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'password',
+      tip: {
+        password_confirmation: 'secret',
+      },
+      root: {},
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [])
+  })
+
+  test('report when using nested fields', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate('secret', compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'user.password',
+      tip: {
+        password_confirmation: 'supersecret',
+      },
+      root: {
+        user: {
+          password_confirmation: 'supersecret',
+        },
+      },
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [{
+      field: 'user.password_confirmation',
+      rule: 'confirmed',
+      message: 'confirmed validation failed',
+    }])
+  })
+
+  test('work fine when matches inside nested fields', (assert) => {
+    const reporter = new ApiErrorReporter({}, false)
+    confirmed.validate('secret', compile().compiledOptions, {
+      errorReporter: reporter,
+      field: 'password',
+      pointer: 'user.password',
+      tip: {
+        password_confirmation: 'secret',
+      },
+      root: {
+        user: {
+          password_confirmation: 'secret',
+        },
+      },
+      mutate: () => {},
+    })
+
+    assert.deepEqual(reporter.toJSON(), [])
+  })
+})

2120229c824866cf34c9cb6899122a2ddabada63
Harminder virk <virk.officials@gmail.com>
2020-06-14 15:15:02 +0530

    fix: types

--- benchmarks/validate.ts
+++ benchmarks/validate.ts
@@ -10,10 +10,11 @@
 import { CompilerOutput } from '@ioc:Adonis/Core/Validator'
 
 import * as validations from '../src/Validations'
+import { MessagesBag } from '../src/MessagesBag'
 import { exists, isObject } from '../src/Validator/helpers'
 import { VanillaErrorReporter } from '../src/ErrorReporter/Vanilla'
 const helpers = { exists, isObject }
 
 export function validate (fn: CompilerOutput<any>, data: any) {
-  return fn(data, validations, new VanillaErrorReporter({}, true), helpers)
+  return fn(data, validations, new VanillaErrorReporter(new MessagesBag({}), true), helpers, {})
 }

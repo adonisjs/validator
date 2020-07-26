a2680fa37a10d7366e6719a47f56088f392dc23d
Harminder virk <virk.officials@gmail.com>
2020-07-26 13:16:56 +0530

    style: comment out example code

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,11 @@
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
-import { DateTime } from 'luxon'
-
-const refs = schema.refs({
-	afterDate: DateTime.local(),
-})
-
-const data = schema.create({
-	username: schema.date({}, [rules.after('tomorrow'), rules.after(refs.afterDate)]),
-	profile: schema.object().anyMembers(),
-}).props
+// import { schema, rules } from '@ioc:Adonis/Core/Validator'
+// import { DateTime } from 'luxon'
+//
+// const refs = schema.refs({
+// 	afterDate: DateTime.local(),
+// })
+//
+// const data = schema.create({
+// 	username: schema.date({}, [rules.after('tomorrow'), rules.after(refs.afterDate)]),
+// 	profile: schema.object().anyMembers(),
+// }).props

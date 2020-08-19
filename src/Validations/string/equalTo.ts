a5ede2d0ae51f840f52b4617a0ca2254e7aedeb8
popovicieduard <eduard.popovici@emanuel.ro>
2020-08-19 14:14:38 +0300

    feat(added equalto w/ refs): added equalTo rule w/ refs

--- src/Validations/string/equalTo.ts
+++ src/Validations/string/equalTo.ts
@@ -8,32 +8,49 @@
  */
 
 import { SyncValidation } from '@ioc:Adonis/Core/Validator'
-import { wrapCompile } from '../../Validator/helpers'
+import { wrapCompile, isRef } from '../../Validator/helpers'
 
 const RULE_NAME = 'equalTo'
 const DEFAULT_MESSAGE = 'equalTo validation failed'
+
+type CompileReturnType = { fieldValue?: string; ref?: string }
 
 /**
  * Validation signature for the "equalTo". Non-string values are
  * ignored.
  */
-export const equalTo: SyncValidation<{ fieldValue: string }> = {
-	compile: wrapCompile(RULE_NAME, ['string'], ([equalToValue]) => {
+export const equalTo: SyncValidation<CompileReturnType> = {
+	compile: wrapCompile<CompileReturnType>(RULE_NAME, ['string'], ([equalToValue], _) => {
 		if (!equalToValue || typeof equalToValue !== 'string') {
 			throw new Error(`The "${RULE_NAME}" rule expects equalToValue to be a string`)
+		}
+
+		if (isRef(equalToValue)) {
+			return {
+				compiledOptions: { ref: equalToValue.key },
+			}
 		}
 
 		return {
 			compiledOptions: { fieldValue: equalToValue },
 		}
 	}),
-	validate(value, { fieldValue }, { errorReporter, arrayExpressionPointer, pointer }) {
+	validate(value, compiledOptions, { errorReporter, arrayExpressionPointer, pointer, refs }) {
 		/**
 		 * Ignore non-string values. The user must apply string rule
 		 * to validate string
 		 */
 		if (typeof value !== 'string') {
 			return
+		}
+
+		let fieldValue
+
+		if (compiledOptions.ref) {
+			const runtimeFieldValue = refs[compiledOptions.ref].value
+			fieldValue = runtimeFieldValue
+		} else if (compiledOptions.fieldValue) {
+			fieldValue = compiledOptions.fieldValue
 		}
 
 		if (value !== fieldValue) {

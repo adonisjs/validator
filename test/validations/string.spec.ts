339323a0c8e1486a0c90aefb5473570791b2befb
Chirill Ceban <cc@bitkidd.dev>
2021-06-09 14:45:09 +0300

    feat: add case, singularize, pluralize and condenseWhitespace options to string
    - Add case (lowerCase, upperCase, camelCase, snakeCase, dashCase, pascalCase, capitalCase, sentenceCase, dotCase, dotCase, titleCase, noCase) options.
    - Add pluralize, singularize and condenseWhitespace options

--- test/validations/string.spec.ts
+++ test/validations/string.spec.ts
@@ -13,8 +13,9 @@ import { validate } from '../fixtures/rules/index'
 import { MessagesBag } from '../../src/MessagesBag'
 import { ApiErrorReporter } from '../../src/ErrorReporter'
 import { string } from '../../src/Validations/primitives/string'
+import { StringOptions } from '@ioc:Adonis/Core/Validator'
 
-function compile(options?: { escape: true }) {
+function compile(options?: StringOptions) {
   return string.compile('literal', 'string', rules['string'](options).options, {})
 }
 
@@ -80,6 +81,244 @@ test.group('String', () => {
     })
 
     assert.deepEqual(reporter.toJSON(), { errors: [] })
+  })
+
+  test('lowerCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello-World'
+
+    string.validate(value, compile({ case: 'lowerCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'hello-world')
+  })
+
+  test('upperCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello-World'
+
+    string.validate(value, compile({ case: 'upperCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'HELLO-WORLD')
+  })
+
+  test('camelCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello-World'
+
+    string.validate(value, compile({ case: 'camelCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'helloWorld')
+  })
+
+  test('snakeCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello-World'
+
+    string.validate(value, compile({ case: 'snakeCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'hello_world')
+  })
+
+  test('dashCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello World'
+
+    string.validate(value, compile({ case: 'dashCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'hello-world')
+  })
+
+  test('pascalCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'hello world'
+
+    string.validate(value, compile({ case: 'pascalCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'HelloWorld')
+  })
+
+  test('capitalCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'hello world'
+
+    string.validate(value, compile({ case: 'capitalCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'Hello World')
+  })
+
+  test('sentenceCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Hello World'
+
+    string.validate(value, compile({ case: 'sentenceCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'Hello world')
+  })
+
+  test('noCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'hello.world'
+
+    string.validate(value, compile({ case: 'noCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'hello world')
+  })
+
+  test('titleCase string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'Here is a fox'
+
+    string.validate(value, compile({ case: 'titleCase' }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'Here Is a Fox')
+  })
+
+  test('pluralize string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'post'
+
+    string.validate(value, compile({ pluralize: true }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'posts')
+  })
+
+  test('singularize string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'posts'
+
+    string.validate(value, compile({ singularize: true }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'post')
+  })
+
+  test('trim string when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = ' untrimmed string '
+
+    string.validate(value, compile({ trim: true }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'untrimmed string')
+  })
+
+  test('condense white space when enabled', (assert) => {
+    const reporter = new ApiErrorReporter(new MessagesBag({}), false)
+    let value = 'uncondensed  string'
+
+    string.validate(value, compile({ condenseWhitespace: true }).compiledOptions, {
+      errorReporter: reporter,
+      field: 'username',
+      pointer: 'username',
+      tip: {},
+      root: {},
+      refs: {},
+      mutate: (newValue) => (value = newValue),
+    })
+
+    assert.equal(value, 'uncondensed string')
   })
 
   test('escape string when enabled', (assert) => {

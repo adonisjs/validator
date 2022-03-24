50b1d8402dc20a36e3f5b5bc271a21ac8f7e73b4
Harminder Virk <virk.officials@gmail.com>
2022-03-24 18:11:29 +0530

    chore: update dependencies

--- bin/japaTypes.ts
+++ bin/japaTypes.ts
@@ -1,12 +1,2 @@
-import { Assert } from '@japa/assert'
-
-declare module '@japa/runner' {
-  interface TestContext {
-    // notify TypeScript about custom context properties
-    assert: Assert
-  }
-
-  interface Test<Context, TestData> {
-    // notify TypeScript about custom test properties
-  }
-}
+import '@japa/assert'
+declare module '@japa/runner' {}

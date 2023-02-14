5cb3365140d6727d76ae864ff977203cbb622f06
Harminder Virk <virk.officials@gmail.com>
2023-02-14 10:45:02 +0530

    style: update license docblock

--- index.ts
+++ index.ts
@@ -6,7 +6,3 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-export { ApiErrorReporter } from './api.js'
-export { VanillaErrorReporter } from './vanilla.js'
-export { JsonApiErrorReporter } from './json_api.js'
